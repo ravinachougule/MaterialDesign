18307834acfc256830946b2d52d8cc4d637b94e7
ravinachougule <ravinachougule7@gmail.com>
2018-04-24 08:24:47 +0530

    Material Design

---
+++ app/src/main/java/com/example/lenovo/materialdesigndemo/Main2Activity.kt
@@ -0,0 +1,21 @@
+package com.example.lenovo.materialdesigndemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
